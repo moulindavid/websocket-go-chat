84f0bf9f81a02b5b8b3242b793af1ec53c38efdc
moulindavid <moulin.david@live.fr>
2023-05-31 18:30:41 +0200

    feat: basic http server

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+    "errors"
+    "fmt"
+    "io"
+    "net/http"
+    "os"
+)
+
+
+func getRoot(w http.ResponseWriter, r *http.Request){
+    fmt.Printf("got / request\n")
+    io.WriteString(w, "Webserver bip bop.. \n")
+}
+
+func getHello(w http.ResponseWriter, r *http.Request){
+    fmt.Printf("got / hello request\n")
+    io.WriteString(w, "HTTP Hello !\n")
+}
+
+func main() {
+	http.HandleFunc("/", getRoot)
+	http.HandleFunc("/hello", getHello)
+
+	err := http.ListenAndServe(":3333", nil)
+
+    if errors.Is(err, http.ErrServerClosed) {
+		fmt.Printf("server closed\n")
+	} else if err != nil {
+		fmt.Printf("error starting server: %s\n", err)
+		os.Exit(1)
+    }
+}
